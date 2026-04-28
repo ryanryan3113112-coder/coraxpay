f3e8da2ea8210ca5e0709feae8342205fd1eddf5
ryanryan3113112-coder <ryanryan3113112@gmail.com>
2026-04-28 22:43:31 +0800

    Create 正式檔案.lua

---
+++ 正式檔案.lua
@@ -0,0 +1,35 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "CrowScript",
+    Icon = 0,
+    LoadingTitle = "載入中...",
+    LoadingSubtitle = "通用版 | 穩定執行",
+    ShowText = "Rayfield", -- for mobile users to unhide Rayfield, change if you'd like
+    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+ 
+    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+ 
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from emitting warnings when the script has a version mismatch with the interface.
+ 
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = RivalsUniversalUI, -- Create a custom folder for your hub/game
+       FileName = "Config"
+    },
+ 
+    Discord = {
+       Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "VDCeKw5DKm", -- The Discord invite code, do not include Discord.gg/. E.g. Discord.gg/ABCD would be ABCD
+       RememberJoins = false -- Set this to false to make them join the Discord every time they load it up
+    },
+ })
+
+ local CombatTab = Window:CreateTab("Combat", "swords")
+ local VisualsTab = Window:CreateTab("Visuals", "eye")
+ local MovementTab = Window:CreateTab("Movement", "footprints")
+ local MiscTab = Window:CreateTab("Misc", "menu")
+ local SettingsTab = Window:CreateTab("Settings", "settings")
+
+Rayfield:LoadConfiguration()
